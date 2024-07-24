e35830c0dcb9b49a7febbd77de6b6a52ad2f069e
Maksim Matveychuk <112119320+MaksimMatveychuk@users.noreply.github.com>
2024-07-24 16:45:27 +0300

    Update create_docker_image.tf

--- backTestDeploy/create_docker_image.tf
+++ backTestDeploy/create_docker_image.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "docker_packaging" {
 
   provisioner "local-exec" {
-    working_dir = "./"
+    working_dir = "backend"
     command     = <<EOF
 aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin ${data.aws_caller_identity.current.account_id}.dkr.ecr.eu-north-1.amazonaws.com && docker build -t backrepository . && docker tag backrepository:latest ${data.aws_caller_identity.current.account_id}.dkr.ecr.eu-north-1.amazonaws.com/backrepository:latest && docker push ${data.aws_caller_identity.current.account_id}.dkr.ecr.eu-north-1.amazonaws.com/backrepository:latest
     EOF
