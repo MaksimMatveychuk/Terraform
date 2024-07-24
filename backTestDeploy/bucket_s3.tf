a44435eb14904dbf9d97339e673988068b2c735b
Maksim Matveychuk <112119320+MaksimMatveychuk@users.noreply.github.com>
2024-07-24 16:24:23 +0300

    Update bucket_s3.tf

--- backTestDeploy/bucket_s3.tf
+++ backTestDeploy/bucket_s3.tf
@@ -1,15 +1,1 @@
-terraform {
-  backend "s3" {
-    bucket = "aws_terraform_states_backend"
-    key    = "backTestDeploy/terraform.tfstate"
-    region = "eu-north-1"
-  }
-}
 
-resource "aws_s3_bucket" "backend_s3_bucket" {
-  bucket = "aws_terraform_states_backend"
-
-  tags = {
-    Name = "aws_terraform_states_backend"
-  }
-}
