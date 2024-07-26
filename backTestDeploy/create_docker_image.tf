1c3d095b20d1845cbc0804491035696bddf52372
NewEnergyCode <matveychuk.m@gmail.com>
2024-07-26 12:46:52 +0300

    try fix problem with docker path

--- backTestDeploy/create_docker_image.tf
+++ backTestDeploy/create_docker_image.tf
@@ -3,7 +3,7 @@ resource "null_resource" "docker_image" {
   provisioner "local-exec" {
     working_dir = "/home/runner/work/Terraform/Terraform/backend"
     command     = <<EOF
-aws ecr get-login-password --region ${var.region} | docker login --username AWS --password-stdin ${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com && docker build -t ${var.back_rep_name} . && docker tag ${var.back_rep_name}:latest ${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com/${var.back_rep_name}:latest && docker push ${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com/${var.back_rep_name}:latest
+aws ecr get-login-password --region ${var.region} | docker login --username AWS --password-stdin ${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com && cd /home/runner/work/Terraform/Terraform/backend && chmod +x ./mvnw && ./mvnw clean package && docker build -t ${var.back_rep_name} . && docker tag ${var.back_rep_name}:latest ${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com/${var.back_rep_name}:latest && docker push ${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com/${var.back_rep_name}:latest
     EOF
   }
 
